97027544972b00016ef6e1ababc43c5032a74ea4
Your Name Here <anupamsurkar@gmail.com>
2020-03-31 12:44:21 +0530

    About us, profile layout.

--- app/src/main/java/com/syrous/yccestudentlibraryjava/ui/profile/FragmentProfile.java
+++ app/src/main/java/com/syrous/yccestudentlibraryjava/ui/profile/FragmentProfile.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.syrous.yccestudentlibraryjava.R;
+import com.syrous.yccestudentlibraryjava.data.User;
 
 /**
  * Created By : Syrous
@@ -19,7 +21,11 @@ public class FragmentProfile extends Fragment {
 
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false );
+
+
+
     }
 }
